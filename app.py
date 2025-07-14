f801580be88117628bd5fdabb9d386ef633c44bf
Teoh TeikToe <teohteiktoe@gmail.com>
2025-07-14 06:32:08 +0000

    1

--- app.py
+++ app.py
@@ -7,16 +7,23 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    # db
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
-
     # load model
     model = joblib.load("dbs.jl")
-
     # make prediction
     pred = model.predict([[q]])
-
     return(render_template("prediction.html",r=pred))
 
 if __name__ == "__main__":
