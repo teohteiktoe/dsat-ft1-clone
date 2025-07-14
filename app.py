aa43ee7fb44d8b89347f56cb3961d38a638ca866
Teoh TeikToe <teohteiktoe@gmail.com>
2025-07-14 04:55:41 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-#
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    q = float(request.form.get("q"))
+
+    # load model
+    model = joblib.load("dbs.jl")
+
+    # make prediction
+    pred = model.predict([[q]])
+
+    return(render_template("prediction.html",r=pred))
+
+if __name__ == "__main__":
+    app.run()
