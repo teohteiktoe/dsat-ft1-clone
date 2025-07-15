1a05a1c971d78a085e5489225e962c0731b3d332
Teoh TeikToe <teohteiktoe@gmail.com>
2025-07-15 11:52:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,26 @@ def llama_reply():
     )
     return(render_template("llama_reply.html",r=completion.choices[0].message.content))
 
+@app.route("/deepseek",methods=["GET","POST"])
+def deepseek():
+    return(render_template("deepseek.html"))
+
+@app.route("/deepseek_reply",methods=["GET","POST"])
+def deepseek_reply():
+    q = request.form.get("q")
+    # load model
+    client = Groq()
+    completion_ds = client.chat.completions.create(
+        model="deepseek-r1-distill-llama-70b",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("deepseek_reply.html",r=completion_ds.choices[0].message.content))
+
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
     return(render_template("dbs.html"))
