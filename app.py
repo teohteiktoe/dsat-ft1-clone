ee9989026e44fa2e616e855f3f15ba66d43be6d1
Teoh TeikToe <teohteiktoe@gmail.com>
2025-07-16 05:42:37 +0000

    1

--- app.py
+++ app.py
@@ -87,7 +87,21 @@ def telegram():
         status = "The telegram bot is running. Please check with the telegram bot. @your_bot"
     else:
         status = "Failed to start the telegram bot. Please check the logs."
-    return(render_template("telegram.html", status=status))
+    return(render_template("telegram.html", r=status))
+
+@app.route("/stop_telegram",methods=["GET","POST"])
+def telegram():
+    domain_url = 'https://dsat-ft1.onrender.com'
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    webhook_response = requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+    # Set the webhook URL for the Telegram bot
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot has stop."
+    else:
+        status = "Failed to stop the telegram bot."
+    return(render_template("telegram.html", r=status))
 
 @app.route("/webhook",methods=["GET","POST"])
 def webhook():
